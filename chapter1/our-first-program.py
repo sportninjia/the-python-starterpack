b996c4735864f990020ce01a08f273a6afdf27fe
sportninjia <adarshrajesh101@gmail.com>
2023-02-27 20:51:29 -0600

    my first chapter code checkin

---
+++ chapter1/our-first-program.py
@@ -0,0 +1,22 @@
+#q1 print "I am now bilingual!" to the console
+print("I am now bilingual!")
+
+"""
+q2 on the next three lines
+print the following
+your first and last name
+favorite color
+favorite number
+
+note : multiline comment
+"""
+
+print("Adarsh Rajesh")
+print("Red")
+print("7")
+
+#multiline print
+print("""
+      Is this a comment?
+      yes, this is a multiline comment
+      """)
