5bd079201d34adea25c0ce68aea9e569eb622318
sportninjia <adarshrajesh101@gmail.com>
2023-02-27 21:25:30 -0600

    Started Types

---
+++ chapter2/numbers.py
@@ -0,0 +1,23 @@
+num1 = 100
+num2 = 200
+
+print(num1 + num2)
+
+print(9+9)
+
+#q create two int types
+#store 55 in var1
+#store 105 in var2
+#mutiply var1 and var2 and print
+
+var1 = 55
+var2 = 105 
+
+print(var1 * var2)
+
+#multiply var1 and var2 and store var3
+#print var3
+
+var3 = var1 * var2
+
+print(var3)
