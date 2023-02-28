88b83c8de3987bdc0a98d230af5b2f899d42d5b9
sportninjia <adarshrajesh101@gmail.com>
2023-02-28 16:31:09 -0600

    Updated version

--- chapter2/numbers.py
+++ chapter2/numbers.py
@@ -5,7 +5,7 @@ print(num1 + num2)
 
 print(9+9)
 
-#q create two int types
+#Q create two int types
 #store 55 in var1
 #store 105 in var2
 #mutiply var1 and var2 and print
@@ -21,3 +21,43 @@ print(var1 * var2)
 var3 = var1 * var2
 
 print(var3)
+
+# Q1 
+
+var1 = 4032 
+var2 = 16 
+
+print(var1/var2)
+
+# Q2 
+
+var1 = 34 
+var2 = 8 
+
+print(var1 * var2)
+
+# Q3
+
+var1 = 92 
+var2 = 1002 
+
+print(var1 + var2)
+
+# Q4 
+
+var1 = 39948
+var2 = 4932 
+
+print(var1 - var2)
+
+# Q5 
+
+var1 = 209948 
+var2 = 45586
+
+# Q5 
+
+var1 = 1002 
+var2 = 53 
+
+print(1002 % 53)
